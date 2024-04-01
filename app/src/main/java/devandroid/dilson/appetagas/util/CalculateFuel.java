fb7c88c01c79537ce9105346f298e104a3e8dc45
Dilson <dilsono316@gmail.com>
2024-04-01 13:01:45 -0300

    add class util

--- app/src/main/java/devandroid/dilson/appetagas/util/CalculateFuel.java
+++ app/src/main/java/devandroid/dilson/appetagas/util/CalculateFuel.java
@@ -1,4 +1,17 @@
 package devandroid.dilson.appetagas.util;
 
+import devandroid.dilson.appetagas.model.Ethanol;
+import devandroid.dilson.appetagas.model.Gasoline;
+
 public class CalculateFuel {
+
+    public static String getResult(Gasoline gasoline, Ethanol ethanol){
+        final Double IDEAL_PRICE = gasoline.getPrice() * 0.7;
+
+        if (ethanol.getPrice() <= IDEAL_PRICE)
+           return "Abastecer com Ethanol";
+
+        return "Abastecer com Gasolina";
+
+    }
 }
