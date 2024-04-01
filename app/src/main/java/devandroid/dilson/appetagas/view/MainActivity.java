048234707453c2f97da5ca565a1f26efd9b1315e
Dilson <dilsono316@gmail.com>
2024-04-01 13:20:11 -0300

    add widgets view

--- app/src/main/java/devandroid/dilson/appetagas/view/MainActivity.java
+++ app/src/main/java/devandroid/dilson/appetagas/view/MainActivity.java
@@ -1,6 +1,10 @@
 package devandroid.dilson.appetagas.view;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +15,12 @@ import androidx.core.view.WindowInsetsCompat;
 import devandroid.dilson.appetagas.R;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editGasoline;
+    private EditText editEthanol;
+    private Button btnCalculate;
+    private Button btnFinish;
+    private Button btnClear;
+    private Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,21 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        this.editEthanol = findViewById(R.id.editEthanol);
+        this.editGasoline = findViewById(R.id.editGasoline);
+        this.btnCalculate = findViewById(R.id.btnCalculate);
+        this.btnClear = findViewById(R.id.btnClear);
+        this.btnFinish = findViewById(R.id.btnFinish);
+        this.btnSave = findViewById(R.id.btnSave);
+
+
+        this.btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Boa Economia!",Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
 
     }
 }
